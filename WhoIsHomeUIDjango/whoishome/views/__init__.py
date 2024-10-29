c59a72b57d9f5acfeae0eb55192f6c82d720aabe
DartLazer <71255041+DartLazer@users.noreply.github.com>
2024-10-29 22:06:17 +0100

    Commit __init__.py for new views directory

---
+++ WhoIsHomeUIDjango/whoishome/views/__init__.py
@@ -0,0 +1,6 @@
+from .settings_view import *
+from .host_detail_view import *
+from .home_page_view import *
+from .network_timeline_view import *
+from .scan_view import *
+from .authentication_view import *
